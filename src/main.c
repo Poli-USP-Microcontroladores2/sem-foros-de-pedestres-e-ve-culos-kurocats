4119c9673fe1d449dd0b1da4ee6c5923b073fe6a
Anita Cunhao <anita.starkiller@gmail.comm>
2025-11-04 13:55:22 -0300

    finalizacao da lógica base do pedestre

--- src/main.c
+++ src/main.c
@@ -63,10 +63,42 @@ void pisca_red(){
 }
 
 // Define as threads de controle de cada uma das cores do semáforo (verde, vermelho e amarelo, respectivamente)
-K_THREAD_DEFINE(luz_verde, STACK_SIZE, pisca_green, NULL, NULL, NULL, PRIORITY, 0, 0);
-K_THREAD_DEFINE(luz_vermelha, STACK_SIZE, pisca_red, NULL, NULL, NULL, PRIORITY, 0, 10);
+K_THREAD_DEFINE(luz_verde, STACK_SIZE, pisca_green, NULL, NULL, NULL, PRIORITY, 0, 10);
+K_THREAD_DEFINE(luz_vermelha, STACK_SIZE, pisca_red, NULL, NULL, NULL, PRIORITY, 0, 0);
 
 void main(void)
 {
+
+    // Verifica se os LEDs estão prontos para uso
+    if (!device_is_ready(ledVerde.port)) {
+        printk("Led verde não está pronto para inicialização");
+        return;
+    }
+
+    if (!device_is_ready(ledVermelho.port)) {
+        printk("Led vermelho não está pronto para inicialização");
+        return;
+    }
+
+    // Ativa os LEDS como output inativo (ativa os LEDS em estado desligado)
+    gpio_pin_configure_dt(&ledVerde, GPIO_OUTPUT_INACTIVE);
+    gpio_pin_configure_dt(&ledVermelho, GPIO_OUTPUT_INACTIVE);
+
+    if (noturno == 1){
+        printk("Modo noturno ativo!\n");
+    
+        while(1){
+            // Loop principal vazio, as threads cuidam do funcionamento dos LED
+            // Caso o modo noturno esteja ativo, pisca o LED vermelho a cada 1 segundo durante a duração da thread do LED vermelho 
+                if(gpio_pin_get_dt(&ledVermelho) == 1){
+                    gpio_pin_toggle_dt(&ledVermelho);
+                    k_msleep(1000);
+                    gpio_pin_toggle_dt(&ledVermelho);
+                    k_msleep(1000);
+                } 
+
+        }
+
+    }
 
 }
